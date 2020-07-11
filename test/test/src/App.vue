135b5af546f7b229aeec4f73f407d6dcb2d3ed4b
Hector Fernandez <hecferme@gmail.com>
2020-07-11 03:00:29 +0000

    App consultando datos alambrados

---
+++ test/test/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <Docs v-bind:items="items"/>
+  </div>
+</template>
+
+<script>
+import  Docs from './components/Docs';
+
+export default {
+  name: 'App',
+  components: {
+    Docs
+  }, 
+  data: () => 
+  { 
+    return { 
+      items: 
+      [ 
+        { 
+          id: 1, 
+          name: "Test 1", 
+          exp: "16 Oct 2022" 
+        },
+        { 
+          id: 2, 
+          name: "Test 2", 
+          exp: "16 Nov 2021" 
+        } 
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ test/test/src/components/Docs.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>DocExpire</h1>
+        <div v-bind:key="item.id" v-for="item in items">
+            <h3>{{item.name}}</h3>
+                <p>{{item.exp}}</p>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Docs",
+    props: ["items"]
+}
+</script>
+
+<style scoped>
+
+</style>
